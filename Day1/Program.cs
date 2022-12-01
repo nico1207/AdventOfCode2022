ee65b16f62dc5a63383b88a580a5c7b1dda158ce
Nico Thomas <thomasnico@web.de>
2022-12-01 20:26:45 +0100

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,9 @@
+﻿var input = File.ReadAllText("input.txt");
+
+// Part 1
+var calories = input.Split("\n\n").Select(g => g.Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Sum());
+Console.WriteLine(calories.Max());
+
+// Part 2
+var top3Calories = calories.OrderDescending().Take(3);
+Console.WriteLine(top3Calories.Sum());
