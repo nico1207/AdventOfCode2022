fdbd5a79757de9a7fdcda541fc7d8b9fbd4d26e8
Nico Thomas <thomasnico@web.de>
2022-12-02 11:27:49 +0100

    Clean up part 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,32 +1,17 @@
 ﻿var input = File.ReadAllLines("input.txt");
-var part1Dic = new Dictionary<string, int>
+
+var dic = new Dictionary<string, (int part1, int part2)>
 {
-    ["A X"] = 3+1,
-    ["B X"] = 0+1, 
-    ["C X"] = 6+1,
-    ["A Y"] = 6+2,
-    ["B Y"] = 3+2, 
-    ["C Y"] = 0+2,
-    ["A Z"] = 0+3,
-    ["B Z"] = 6+3, 
-    ["C Z"] = 3+3,
+    ["A X"] = (3+1, 0+3),
+    ["B X"] = (0+1, 0+1),
+    ["C X"] = (6+1, 0+2),
+    ["A Y"] = (6+2, 3+1),
+    ["B Y"] = (3+2, 3+2),
+    ["C Y"] = (0+2, 3+3),
+    ["A Z"] = (0+3, 6+2),
+    ["B Z"] = (6+3, 6+3),
+    ["C Z"] = (3+3, 6+1)
 };
 
-var part1Result = input.Sum(x => part1Dic[x]);
-Console.WriteLine(part1Result);
-
-var part2Dic = new Dictionary<string, int>
-{
-    ["A X"] = 0+3,
-    ["B X"] = 0+1, 
-    ["C X"] = 0+2,
-    ["A Y"] = 3+1,
-    ["B Y"] = 3+2, 
-    ["C Y"] = 3+3,
-    ["A Z"] = 6+2,
-    ["B Z"] = 6+3, 
-    ["C Z"] = 6+1,
-};
-
-var part2Result = input.Sum(x => part2Dic[x]);
-Console.WriteLine(part2Result);
+Console.WriteLine(input.Sum(x => dic[x].part1));
+Console.WriteLine(input.Sum(x => dic[x].part2));
