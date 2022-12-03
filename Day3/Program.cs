c404e597ea375a7a2fc7b96ece2b73a4fd114eba
Nico Thomas <thomasnico@web.de>
2022-12-03 21:34:38 +0100

    Day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,7 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+var priorities = input.Select(line => new[] {..(line.Length/2), (line.Length/2)..}.Select(range => line[range]).ToArray()).Select(halves => halves[0].Intersect(halves[1]).Single() - 'a').Select(chr => chr < 0 ? chr + 32+26+1 : chr+1);
+Console.WriteLine(priorities.Sum());
+
+var badgePriorities = input.Chunk(3).Select(group => group.ToArray()).Select(group => group[0].Intersect(group[1]).Intersect(group[2]).Single() - 'a').Select(chr => chr < 0 ? chr + 32+26+1 : chr+1);
+Console.WriteLine(badgePriorities.Sum());
