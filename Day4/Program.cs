997d04a0bbc3287dd1cc2b3409204f9c69f6377b
Nico Thomas <thomasnico@web.de>
2022-12-04 15:53:38 +0100

    Remove that newline, wtf

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,6 +1,5 @@
 ﻿var input = File.ReadAllLines("input.txt");
-var parsedInput =
-    input.Select(line => line.Split(',', '-').Select(int.Parse).ToArray());
+var parsedInput = input.Select(line => line.Split(',', '-').Select(int.Parse).ToArray());
 
 Console.WriteLine(parsedInput.Count(a => (a[0] >= a[2] && a[1] <= a[3]) || (a[2] >= a[0] && a[3] <= a[1])));
 Console.WriteLine(parsedInput.Count(a => (a[0] >= a[2] && a[0] <= a[3]) || (a[2] >= a[0] && a[2] <= a[1])));
