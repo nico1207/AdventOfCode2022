8d54411a3fd54403c29e2623915921bc64dbbe2e
Nico Thomas <thomasnico@web.de>
2022-12-04 12:53:18 +0100

    Day 4

---
+++ Day4/Program.cs
@@ -0,0 +1,7 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+var fullOverlaps = input.Select(line => line.Split(",").Select(ass => ass.Split("-").Select(int.Parse).ToArray()).ToArray()).Where(a => (a[0][0] >= a[1][0] && a[0][1] <= a[1][1]) || (a[1][0] >= a[0][0] && a[1][1] <= a[0][1]));
+Console.WriteLine(fullOverlaps.Count());
+
+var anyOverlaps = input.Select(line => line.Split(",").Select(ass => ass.Split("-").Select(int.Parse).ToArray()).ToArray()).Where(a => (a[0][0] >= a[1][0] && a[0][0] <= a[1][1]) || (a[1][0] >= a[0][0] && a[1][0] <= a[0][1]));
+Console.WriteLine(anyOverlaps.Count());
