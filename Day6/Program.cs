6c6e37c2587410ee07de27587431361f8ce2c768
Nico Thomas <thomasnico@web.de>
2022-12-06 11:36:27 +0100

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,9 @@
+﻿using MoreLinq;
+
+var input = File.ReadAllText("input.txt");
+
+var marker1 = input.Window(4).ToList().FindIndex(w => w.Distinct().Count() == 4);
+Console.WriteLine(marker1+4);
+
+var marker2 = input.Window(14).ToList().FindIndex(w => w.Distinct().Count() == 14);
+Console.WriteLine(marker2+14);
