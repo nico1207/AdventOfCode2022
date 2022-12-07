ed183ee16204e673cd02248ed5b66d7bc37672b5
Nico Thomas <thomasnico@web.de>
2022-12-07 14:38:01 +0100

    Use Scan for prefixes

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,6 +1,6 @@
 using System.Collections.Immutable;
 using Medallion.Collections;
-
+using MoreLinq.Extensions;
 using List = System.Collections.Immutable.ImmutableList<string>;
 using Dict = System.Collections.Immutable.ImmutableDictionary<System.Collections.Immutable.ImmutableList<string>, long>;
 
@@ -9,11 +9,11 @@ var data = File.ReadAllLines("input.txt").Aggregate((dict: Dict.Empty.SetItem(Li
     ["dir", _] or ["$", "ls"] or ["$", "cd", "/"] => i,
     ["$", "cd", ".."] => (i.dict, i.curDir.RemoveAt(i.curDir.Count - 1)),
     ["$", "cd", var path] => (i.dict.SetItem(i.curDir.Add(path), 0), i.curDir.Add(path)),
-    [var size, _] => (i.dict.SetItems(Enumerable.Range(0, i.curDir.Count+1).Select(j => i.curDir.Take(j).ToImmutableList()).Select(k => new KeyValuePair<List, long>(k, i.dict[k] + long.Parse(size)))), i.curDir)
+    [var size, _] => (i.dict.SetItems(i.curDir.Scan(List.Empty, (l, s) => l.Add(s)).Select(k => new KeyValuePair<List, long>(k, i.dict[k] + long.Parse(size)))), i.curDir)
 });
 
 Console.WriteLine(data.dict.Where(s => s.Value <= 100000).Sum(s => s.Value));
 
 var spaceNeeded = 30000000 - (70000000 - data.dict[List.Empty]);
-var firstFile = data.dict.Where(s => s.Value > spaceNeeded).MinBy(s => s.Value);
+var firstFile = Enumerable.MinBy(data.dict.Where(s => s.Value > spaceNeeded), s => s.Value);
 Console.WriteLine(firstFile.Value);
