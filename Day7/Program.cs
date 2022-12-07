8b1dbf9f215e11a888b6a20c68cd86eac70388ac
Nico Thomas <thomasnico@web.de>
2022-12-07 10:54:25 +0100

    Use pattern matching in Day 7

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,28 +1,26 @@
 ﻿Dictionary<string, long> sizes = new Dictionary<string, long>();
 string currentPath = "";
 
-foreach (var line in File.ReadAllLines("input.txt"))
+foreach (var line in File.ReadAllLines("input.txt").Select(l => l.Split(" ")))
 {
-    if (line.StartsWith("$ cd "))
+    switch (line)
     {
-        currentPath = Path.GetFullPath(Path.Combine(currentPath, line.Substring(5)));
-        sizes.TryAdd(currentPath, 0);
-    } 
-    else if (line.StartsWith("$ ls") || line.StartsWith("dir "))
-    {
-        // ignore
-    }
-    else
-    {
-        var parts = line.Split(" ");
-        var size = int.Parse(parts[0]);
-        string directory = currentPath;
-        while (directory != @"C:\")
-        {
-            sizes[directory] += size;
-            directory = Path.GetFullPath(Path.Combine(directory, ".."));
-        }
-        sizes[directory] += size; // Add to root
+        case ["dir", _]:
+        case ["$", "ls"]:
+            break;
+        case ["$", "cd", var path]:
+            currentPath = Path.GetFullPath(Path.Combine(currentPath, path));
+            sizes.TryAdd(currentPath, 0);
+            break;
+        case [var size, _]:
+            string directory = currentPath;
+            while (directory != @"C:\")
+            {
+                sizes[directory] += long.Parse(size);
+                directory = Path.GetFullPath(Path.Combine(directory, ".."));
+            }
+            sizes[directory] += long.Parse(size); // Add to root
+            break;
     }
 }
 
