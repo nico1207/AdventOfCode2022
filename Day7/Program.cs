882a171b74dd6e71bfd26a0214340de45ac5fbc0
Nico Thomas <thomasnico@web.de>
2022-12-07 15:40:08 +0100

    Revert to fancy solution

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,19 +1,24 @@
-using System.Collections.Immutable;
-using Medallion.Collections;
-using MoreLinq.Extensions;
-using List = System.Collections.Immutable.ImmutableList<string>;
-using Dict = System.Collections.Immutable.ImmutableDictionary<System.Collections.Immutable.ImmutableList<string>, long>;
+var sizes = new Dictionary<string, long>();
+var currentPath = "";
 
-var data = File.ReadAllLines("input.txt").Aggregate((dict: Dict.Empty.SetItem(List.Empty, 0).WithComparers(EqualityComparers.GetSequenceComparer<string>()), curDir: List.Empty), (i, s) => s.Split(" ") switch
+foreach (var line in File.ReadAllLines("input.txt"))
 {
-    ["dir", _] or ["$", "ls"] or ["$", "cd", "/"] => i,
-    ["$", "cd", ".."] => (i.dict, i.curDir.RemoveAt(i.curDir.Count - 1)),
-    ["$", "cd", var path] => (i.dict.SetItem(i.curDir.Add(path), 0), i.curDir.Add(path)),
-    [var size, _] => (i.dict.SetItems(i.curDir.Scan(List.Empty, (l, s) => l.Add(s)).Select(k => new KeyValuePair<List, long>(k, i.dict[k] + long.Parse(size)))), i.curDir)
-});
+    if (line.StartsWith("$ cd "))
+    {
+        currentPath = Path.GetFullPath(Path.Combine(currentPath, line[5..]));
+        sizes.TryAdd(currentPath, 0);
+    } 
+    else if (char.IsDigit(line[0]))
+    {
+        var size = int.Parse(line[..line.IndexOf(' ')]);
+        var directory = currentPath;
+        while (directory != "")
+        {
+            sizes[directory] += size;
+            directory = directory == @"C:\" ? "" : Path.GetFullPath(Path.Combine(directory, ".."));
+        }
+    }
+}
 
-Console.WriteLine(data.dict.Where(s => s.Value <= 100000).Sum(s => s.Value));
-
-var spaceNeeded = 30000000 - (70000000 - data.dict[List.Empty]);
-var firstFile = Enumerable.MinBy(data.dict.Where(s => s.Value > spaceNeeded), s => s.Value);
-Console.WriteLine(firstFile.Value);
+Console.WriteLine(sizes.Values.Where(s => s <= 100000).Sum(s => s));
+Console.WriteLine(sizes.Values.Where(s => s > sizes[@"C:\"] - 40000000).MinBy(s => s));
