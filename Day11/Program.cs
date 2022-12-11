b60d4f00b2de0a7d8bf4231babbfcc02e15dcebf
Nico Thomas <thomasnico@web.de>
2022-12-11 14:51:10 +0100

    Refactor

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -3,38 +3,40 @@ var monkeys = input.Select(i =>
 {
     var items = i[1][17..].Split(", ").Select(long.Parse).ToList();
     var operation = i[2][19..].Split(' ');
-    var operationFunc = operation switch
+    Func<long, long> operationFunc = operation switch
     {
-        ["old", "+", var n] => (Func<long, long>)(x => x + long.Parse(n)),
-        ["old", "*", "old"] => (Func<long, long>)(x => x * x),
-        ["old", "*", var n] => (Func<long, long>)(x => x * long.Parse(n)),
+        ["old", "+", var n] => x => x + long.Parse(n),
+        ["old", "*", "old"] => x => x * x,
+        ["old", "*", var n] => x => x * long.Parse(n),
     };
     var divisor = int.Parse(i[3][20..]);
     var trueIndex = int.Parse(i[4][28..]);
     var falseIndex = int.Parse(i[5][29..]);
     return new Monkey(items, operationFunc, divisor, trueIndex, falseIndex);
-}).ToList();
+}).ToArray();
 
 long PerformRounds(int rounds, bool part2)
 {
-    var inspections = new long[monkeys.Count];
-    var modulo = monkeys.Select(m => m.testDivisor).Aggregate(1L, (a, b) => a * b);
-    for (int round = 0; round < rounds; round++)
-        foreach (var monkey in monkeys)
-            for (int item = monkey.items.Count-1; item >= 0; item--)
-            {
-                inspections[monkeys.IndexOf(monkey)]++;
-                monkey.items[item] = monkey.operation(monkey.items[item]);
-                monkey.items[item] = part2 ? monkey.items[item] % modulo : monkey.items[item] / 3;
-                var targetIndex = monkey.items[item] % monkey.testDivisor == 0 ? monkey.trueIndex : monkey.falseIndex;
-                monkeys[targetIndex].items.Add(monkey.items[item]);
-                monkey.items.RemoveAt(item);
-            }
-
-    return inspections.OrderDescending().Take(2).Aggregate((long)1, (a, b) => a * b);
+    var inspections = new long[monkeys.Length];
+    var modulo = monkeys.Select(m => m.Divisor).Aggregate(1L, (a, b) => a * b);
+    for (var round = 0; round < rounds; round++)
+    for (var monkeyIndex = 0; monkeyIndex < monkeys.Length; monkeyIndex++)
+    {
+        var monkey = monkeys[monkeyIndex];
+        foreach (var item in monkey.Items)
+        {
+            inspections[monkeyIndex]++;
+            var newItem = monkey.Operation(item) % modulo;
+            if (!part2) newItem /= 3;
+            var targetIndex = newItem % monkey.Divisor == 0 ? monkey.TrueIdx : monkey.FalseIdx;
+            monkeys[targetIndex].Items.Add(newItem);
+        }
+        monkey.Items.Clear();
+    }
+    return inspections.OrderDescending().Take(2).Aggregate(1L, (a, b) => a * b);
 }
 
 Console.WriteLine(PerformRounds(20, false));
 Console.WriteLine(PerformRounds(10000, true));
 
-record Monkey(List<long> items, Func<long, long> operation, int testDivisor, int trueIndex, int falseIndex);
+record Monkey(List<long> Items, Func<long, long> Operation, int Divisor, int TrueIdx, int FalseIdx);
